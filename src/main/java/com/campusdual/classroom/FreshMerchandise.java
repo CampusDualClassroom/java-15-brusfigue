7baeb9808d71b29f521bff07214d4808e10d06b3
Bruno Figueroa Rodriguez <brunofigui@gmail.com>
2024-10-28 15:25:08 +0100

    Ejercicio15acabado

--- src/main/java/com/campusdual/classroom/FreshMerchandise.java
+++ src/main/java/com/campusdual/classroom/FreshMerchandise.java
@@ -5,4 +5,36 @@ import java.util.Date;
 
 public class FreshMerchandise extends Merchandise {
 
+    private Date expirationDate;
+
+
+    public FreshMerchandise(String name, String uniqueId, String responsibleId, int zone, String area, String shelf, int quantity, Date expirationDate) {
+        super(name, uniqueId, responsibleId, zone, area, shelf, quantity);
+        this.expirationDate = expirationDate;
+    }
+
+    @Override
+    public String getSpecificData() {
+        StringBuilder data = new StringBuilder();
+        data.append("Location: ").append(this.getLocation())
+                .append("\nExpiration Date: ").append(getFormattedDate(expirationDate));
+        return data.toString();
+    }
+
+    public void printSpecificData(){
+        System.out.println(getSpecificData());
+    }
+
+    public String getFormattedDate(Date date) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        return dateFormat.format(date);
+    }
+
+    public Date getExpirationDate() {
+        return this.expirationDate;
+    }
+
+    public void setExpirationDate(Date expirationDate) {
+        this.expirationDate = expirationDate;
+    }
 }
